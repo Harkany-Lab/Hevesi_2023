e584a94c5f94f1a4bfb735fd54fcdf708dfb5e5c
Evgenii O. Tretiakov <Evgenii.Tretiakov@meduniwien.ac.at>
2023-03-04 23:33:01 +0000

    workflowr::wflow_publish(c("analysis/eda.Rmd", "analysis/figure_2d-THP7.Rmd",     "analysis/figure_2e-Pr5P7.Rmd"), all = TRUE, update = TRUE,     republish = TRUE, verbose = TRUE)

--- code/render.R
+++ code/render.R
@@ -1,2 +1,6 @@
-workflowr::wflow_publish("analysis/eda.Rmd", all = TRUE, update = TRUE, republish = TRUE, verbose = TRUE)
-# workflowr::wflow_publish("analysis/eda.Rmd", all = TRUE, verbose = TRUE)
+workflowr::wflow_publish(
+  c("analysis/eda.Rmd",
+    "analysis/figure_2d-THP7.Rmd",
+    "analysis/figure_2e-Pr5P7.Rmd"),
+  all = TRUE, update = TRUE, republish = TRUE, verbose = TRUE)
+# workflowr::wflow_publish("analysis/eda.Rmd", all = TRUE, update = TRUE, republish = TRUE, verbose = TRUE)
