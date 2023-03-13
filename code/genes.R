3111deecc267330e3051234f29fa5cebb7c1c6ea
Evgenii O. Tretiakov <Evgenii.Tretiakov@meduniwien.ac.at>
2023-03-13 18:52:20 +0000

    workflowr::wflow_publish(c("analysis/eda.Rmd", "analysis/figure_2d-THP7.Rmd",     "analysis/figure_2e-Pr5P7.Rmd"), all = TRUE, update = TRUE,     republish = TRUE, verbose = TRUE)

--- code/genes.R
+++ code/genes.R
@@ -13,12 +13,29 @@ np <- c('Adcyap1', 'Oxt', 'Avp', 'Tac1', 'Pomc', 'Pnoc', 'Tac2',
         'Npw', 'Npvf', 'Ghrh', 'Grp', 'Vip', 'Nms', 'Nmu', 'Nmb',
         'Kiss1', 'Crh', 'Bdnf', 'Cntf', 'Cck')
 
-neurotrans <- c('Slc17a6', 'Slc17a7', 'Slc17a8', 'Slc1a1', 'Slc1a2', 'Slc1a6',
-                'Gad1', 'Slc32a1', 'Slc6a1')
-glut       <- c('Slc17a6', 'Slc17a7', 'Slc17a8', 'Slc1a1', 'Slc1a2', 'Slc1a6')
-gaba       <- c('Gad1', 'Gad2', 'Slc32a1', 'Slc6a1')
-dopam      <- c('Th', 'Slc6a3', 'Slc18a2', 'Ddc',  'Slc18a3')
-ach        <- c('Chat', 'Slc18a3', 'Ache', 'Slc5a7')
+neurotrans <- c("Slc17a6", "Slc17a7", "Slc17a8", "Slc1a1", "Slc1a2", "Slc1a6",
+                "Gad1", "Slc32a1", "Slc6a1")
+glut       <- c("Slc17a6", "Slc17a7", "Slc17a8", "Slc1a1", "Slc1a2", "Slc1a6")
+glutr      <- c("Gria1", "Gria2", "Gria3", "Gria4", # iGlu AMPA receptors
+                "Grid1", "Grid2", # iGlu delta receptors
+                "Grik1", "Grik2", "Grik3", "Grik4", "Grik5", # iGlu kainate receptors
+                "Grin1", "Grin2a", "Grin2b", "Grin2c", "Grin2d", "Grin3a", "Grin3b", # iGlu NMDA receptors
+                "Grm1", "Grm5", # mGluRs 1
+                "Grm2", "Grm3", # mGluRs 2
+                "Grm4", "Grm6", "Grm7", "Grm8"# mGluRs 3
+)
+gaba       <- c("Gad1", "Gad2", "Slc32a1", "Slc6a1")
+gabar <- c(
+  "Gabra1", "Gabra2", "Gabra3", "Gabra4", "Gabra5", "Gabra6",
+  "Gabrb1", "Gabrb2", "Gabrb3",
+  "Gabrg1", "Gabrg2", "Gabrg3",
+  "Gabrd", "Gabre", "Gabrp", "Gabrq",
+  "Gabrr1", "Gabrr2", "Gabrr3",
+  "Gabbr1", "Gabbr2"
+)
+
+dopam <- c("Th", "Slc6a3", "Slc18a2", "Ddc", "Slc18a3")
+ach <- c("Chat", "Slc18a3", "Ache", "Slc5a7")
 genes.zh   <- c("Rbfox3", "Gja1", "Aqp4", "Slc1a3", "Gfap", "Slc17a6", "Slc17a7",
                 "Gad1", "Gad2", "Slc18a2", "Plcb1", "Prkaca", "Adcy1", "Grin1", "Gap43",
                 "Galr1", "Galr2", "Galr3", "Lmx1b", "Bax", "Dcaf5", "Ipo11", "Ntn1",
