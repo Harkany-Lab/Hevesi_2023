691385abd395a1c2729b4a28f379582e676de194
Evgenii O. Tretiakov <Evgenii.Tretiakov@meduniwien.ac.at>
2023-04-20 10:19:39 +0000

    Build site.

--- code/render.R
+++ code/render.R
@@ -1,6 +1,8 @@
 workflowr::wflow_publish(
-  c("analysis/eda.Rmd",
+  c(
     "analysis/figure_2d-THP7.Rmd",
-    "analysis/figure_2e-Pr5P7.Rmd"),
-  all = TRUE, update = TRUE, republish = TRUE, verbose = TRUE)
+    "analysis/figure_2e-Pr5P7.Rmd"
+  ),
+  verbose = TRUE
+)
 # workflowr::wflow_publish("analysis/eda.Rmd", all = TRUE, update = TRUE, republish = TRUE, verbose = TRUE)
